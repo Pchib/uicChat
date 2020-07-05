32a3922b22ad92ae3e024c56b83d54b2eb8532c6
Pchib <ugwuisaaciu@gmail.com>
2020-07-05 10:33:01 -0700

    second commit

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -19,8 +19,8 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  // const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
-  const ENDPOINT = 'http://localhost:5000';
+   const ENDPOINT = 'https://uicchat.herokuapp.com/';
+  // const ENDPOINT = 'http://localhost:5000';
 
 
   let history = useHistory()
